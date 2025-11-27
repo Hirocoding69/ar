67f931be94d953c451c03604ffa955ac84aaf06f
Hirocoding69 <somborteng1@gmail.com>
2025-11-27 09:59:02 +0700

    fix nig

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import * as THREE from "three";
 import * as ZapparThree from "@zappar/zappar-threejs";
 
-const targetImage1 = new URL(
-  "../assets/two-targets/target1.zpt",
-  import.meta.url
-).href;
-const targetImage2 = new URL(
-  "../assets/two-targets/target2.zpt",
-  import.meta.url
-).href;
+const targetImage1 = new URL("../assets/two-targets/500.zpt", import.meta.url)
+  .href;
+const targetImage2 = new URL("../assets/two-targets/500.zpt", import.meta.url)
+  .href;
 // Single video that will play on both trackers
 const videoUrl = new URL("../assets/video/vid.mp4", import.meta.url).href;
 // ZapparThree provides a LoadingManager that shows a progress bar while
